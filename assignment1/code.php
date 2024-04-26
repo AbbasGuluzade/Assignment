00737bf0a3a0538d7e30484d56228207043be1d5
Abbas Guluzade <abbas.guluzade@im-par.de>
2024-04-26 05:03:07 +0300

    assignment1 finished

--- assignment1/code.php
+++ assignment1/code.php
@@ -1,4 +1,54 @@
 <?php
+
+// ------------------- Helper functions -------------------
+
+// sort the call logs by the timestamp in ascending order
+function sortByFirstElement($a, $b)
+{
+    return $a[0] <=> $b[0]; // Compare first elements of each sub-array
+}
+
+// sort the call logs by the item number in ascending order
+function sortBySecondElement($a, $b)
+{
+    return $a[1] <=> $b[1]; // Compare second elements of each sub-array
+}
+
+function printSortedArray($callLogs)
+{
+    foreach ($callLogs as $subArray) {
+        fwrite(STDOUT, $subArray[0] . ": " . $subArray[1] . PHP_EOL);
+    }
+}
+
+// count the number of occurrences of a value in an array
+function array_count_values_of($value, $array) {
+    $counts = array_count_values($array);
+    return $counts[$value];
+}
+
+
+// decrement all the elements in the array except the ones in the accessedItems array
+function decrementAll($arr, $accessedItems)
+{
+    foreach ($arr as $key => $value) {
+        if (!in_array($key, $accessedItems) && $value > 0) {
+            $arr[$key] = $value - 1;
+        }
+    }
+
+    return $arr;
+}
+
+function updatePriorities(&$priorities, &$cache, $item)
+{
+    if (isset($priorities[$item])) {
+        $priorities[$item] += 2;
+    } else {
+        $cache[$item] += 2;
+    }
+}
+
 // implement cacheContents function accoding to assignment
 function cacheContents(array $callLogs): array
 {
@@ -8,12 +58,15 @@ function cacheContents(array $callLogs): array
 
     // sort the call logs by the timestamp in ascending order
     usort($callLogs, 'sortByFirstElement');
+    printSortedArray($callLogs);
     $accessedItems = [];
     $cache = [];
     $currentTime = 0;
     for ($i = 0; $i < count($callLogs); $i++) {
         // if the current timestamp is greater than the current time, update the current time
         if ($callLogs[$i][0] > $currentTime) {
+
+            $temp = $i;
 
             $currentTime = $callLogs[$i][0];
 
@@ -22,22 +75,21 @@ function cacheContents(array $callLogs): array
 
 
             // push the accessed item to the accessed items array, doesn't matter if it is already there
-            array_push($accessedItems, $callLogs[$i][1]);
+            while ($i < count($callLogs) && $callLogs[$i][0] == $currentTime) {
+                print "Current time: " . $currentTime . "\n";
+                array_push($accessedItems, $callLogs[$i][1]);
+                $i++;
+            }
 
+            $i = $temp;
+
+            foreach ($accessedItems as $item) {
+                updatePriorities($priorities, $cache, $item);
+            }
 
             $priorities = decrementAll($priorities, $accessedItems);
             $cache = decrementAll($cache, $accessedItems);
 
-
-        } else {
-            // push the accessed item to the accessed items array, doesn't matter if it is already there
-            array_push($accessedItems, $callLogs[$i][1]);
-
-        }
-        if (isset($cache[$callLogs[$i][1]])) {
-            $cache[$callLogs[$i][1]] += 2 * array_count_values_of($callLogs[$i][1], $accessedItems);
-        } else {
-            $priorities[$callLogs[$i][1]] += 2 * array_count_values_of($callLogs[$i][1], $accessedItems);
         }
 
         // check all the priorities and move the items to the cache with priority higher than 5
@@ -55,14 +107,6 @@ function cacheContents(array $callLogs): array
                 unset($cache[$key]);
             }
         }
-
-        print "Priorities: ";
-        print_r($priorities);
-        print "\n";
-        print "Cache: ";
-        print_r($cache);
-        print_r("\n");
-        print_r("-------------------\n");
     }
 
     //return the indices of nonzero elements in the cache
@@ -83,40 +127,6 @@ for ($i = 0; $i < $callLogs_rows; $i++) {
     $callLogs_temp = rtrim(fgets(STDIN));
 
     $callLogs[] = array_map('intval', preg_split('/ /', $callLogs_temp, -1, PREG_SPLIT_NO_EMPTY));
-}
-
-// sort the call logs by the timestamp in ascending order
-function sortByFirstElement($a, $b)
-{
-    return $a[0] <=> $b[0]; // Compare first elements of each sub-array
-}
-
-function sortBySecondElement($a, $b)
-{
-    return $a[1] <=> $b[1]; // Compare second elements of each sub-array
-}
-
-function printSortedArray($callLogs)
-{
-    foreach ($callLogs as $subArray) {
-        fwrite(STDOUT, $subArray[0] . ": " . $subArray[1] . PHP_EOL);
-    }
-}
-
-function array_count_values_of($value, $array) {
-    $counts = array_count_values($array);
-    return $counts[$value];
-}
-
-function decrementAll($arr, $accessedItems)
-{
-    foreach ($arr as $key => $value) {
-        if (!in_array($key, $accessedItems) && $value > 0) {
-            $arr[$key] = $value - 1;
-        }
-    }
-
-    return $arr;
 }
 
 
